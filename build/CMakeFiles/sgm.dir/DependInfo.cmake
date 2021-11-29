401d6e0cdc879a9fd43c4e9db0b2cc76c6349e00
finley_mac <finleyyang@163.com>
2021-11-29 11:43:57 +0800

    add code mac

--- build/CMakeFiles/sgm.dir/DependInfo.cmake
+++ build/CMakeFiles/sgm.dir/DependInfo.cmake
@@ -4,36 +4,17 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/finley/CODE/sgm/Camera.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/Camera.cpp.o"
-  "/home/finley/CODE/sgm/Image.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/Image.cpp.o"
-  "/home/finley/CODE/sgm/Plane.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/Plane.cpp.o"
-  "/home/finley/CODE/sgm/Ray.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/Ray.cpp.o"
-  "/home/finley/CODE/sgm/common.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/common.cpp.o"
-  "/home/finley/CODE/sgm/main.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/main.cpp.o"
-  "/home/finley/CODE/sgm/sgm.cpp" "/home/finley/CODE/sgm/build/CMakeFiles/sgm.dir/sgm.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "CGAL_USE_GMP"
-  "CGAL_USE_GMPXX"
-  "CGAL_USE_MPFR"
-  )
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "."
-  "/usr/include/eigen3"
-  "/usr/local/include/opencv4"
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/finley/Desktop/course/CODE/sgm/Camera.cpp" "CMakeFiles/sgm.dir/Camera.cpp.o" "gcc" "CMakeFiles/sgm.dir/Camera.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/Image.cpp" "CMakeFiles/sgm.dir/Image.cpp.o" "gcc" "CMakeFiles/sgm.dir/Image.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/Plane.cpp" "CMakeFiles/sgm.dir/Plane.cpp.o" "gcc" "CMakeFiles/sgm.dir/Plane.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/Ray.cpp" "CMakeFiles/sgm.dir/Ray.cpp.o" "gcc" "CMakeFiles/sgm.dir/Ray.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/common.cpp" "CMakeFiles/sgm.dir/common.cpp.o" "gcc" "CMakeFiles/sgm.dir/common.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/main.cpp" "CMakeFiles/sgm.dir/main.cpp.o" "gcc" "CMakeFiles/sgm.dir/main.cpp.o.d"
+  "/Users/finley/Desktop/course/CODE/sgm/sgm.cpp" "CMakeFiles/sgm.dir/sgm.cpp.o" "gcc" "CMakeFiles/sgm.dir/sgm.cpp.o.d"
   )
 
 # Targets to which this target links.
