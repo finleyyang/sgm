89d94cddeff2217b09646ae307328ee75f2cb818
finley_smartmore_desktop <finleyyang@163.com>
2021-11-30 16:17:02 +0800

    add code

--- build/CMakeFiles/sgm.dir/cmake_clean.cmake
+++ build/CMakeFiles/sgm.dir/cmake_clean.cmake
@@ -1,18 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/sgm.dir/Camera.cpp.o"
-  "CMakeFiles/sgm.dir/Camera.cpp.o.d"
   "CMakeFiles/sgm.dir/Image.cpp.o"
-  "CMakeFiles/sgm.dir/Image.cpp.o.d"
   "CMakeFiles/sgm.dir/Plane.cpp.o"
-  "CMakeFiles/sgm.dir/Plane.cpp.o.d"
   "CMakeFiles/sgm.dir/Ray.cpp.o"
-  "CMakeFiles/sgm.dir/Ray.cpp.o.d"
   "CMakeFiles/sgm.dir/common.cpp.o"
-  "CMakeFiles/sgm.dir/common.cpp.o.d"
   "CMakeFiles/sgm.dir/main.cpp.o"
-  "CMakeFiles/sgm.dir/main.cpp.o.d"
   "CMakeFiles/sgm.dir/sgm.cpp.o"
-  "CMakeFiles/sgm.dir/sgm.cpp.o.d"
   "sgm"
   "sgm.pdb"
 )
